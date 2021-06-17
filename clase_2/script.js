2e54bee3ae4dfa4acf9197e43032cfd92dbcb758
Mauricio Romero <mauricionromero@hotmail.com>
2021-06-17 18:59:13 -0300

    añadida clase 2

---
+++ clase_2/script.js
@@ -0,0 +1,75 @@
+//VARIABLES GLOBALES
+const Formulario = document.getElementById("formulario");
+const Listado = document.getElementById("lista");
+let lista = [];
+
+
+
+//FUNCIONES
+function guardar(e) {
+    lista.push(e);
+}
+
+function listar() {
+    if (lista) {
+        lista.forEach(tarea => {
+            let item = document.createElement("div");
+            item.className = "tarea";
+
+            item.innerHTML = `
+            <div class="tarea-cabecera">
+                <p class="tarea-titulo">${tarea.titulo}</p>
+                <p class="tarea-tiempo"> ${tarea.tiempo}m</p>
+                <span class="material-icons tarea-importante ${tarea.esImportante ? '' : 'oculta'}"> priority_high </span>
+            </div>
+            <div class="tarea-cuerpo">
+                <p class="tarea-descripcion">${tarea.descripcion}</p>
+                <div class="tarea-botones">
+                    <span id="hecho" class="material-icons"> check </span>
+                    <span id="editar" class="material-icons"> edit </span>
+                    <span id="borrar" class="material-icons"> delete </span>
+                </div>
+            </div>
+            <span class="material-icons tarea-expandir" onclick="expandir(this)">
+                expand_less
+            </span>`;
+            Listado.appendChild(item);
+        })
+    }
+}
+
+let expandir = (e) => {
+    let cuerpo = e.parentElement.querySelector(".tarea-cuerpo"); 
+    let seMuestra = cuerpo.classList.contains("oculta");
+    if (seMuestra){
+        cuerpo.classList.remove("oculta");
+    } else {
+        cuerpo.classList.add("oculta");
+    }
+}
+
+
+
+
+//EVENT LISTENERS
+Formulario.addEventListener('submit', e => {
+    e.preventDefault();
+
+    let data = e.target
+
+    let esImportante = data.querySelector('#importante').checked;
+    let titulo = data.querySelector('#titulo').value;
+    let descripcion = data.querySelector('#descripcion').value;
+    let tiempo = data.querySelector('input[name="tiempo"]:checked').value;
+
+    let tarea = {
+        esImportante,
+        titulo,
+        descripcion,
+        tiempo
+    }
+
+    guardar(tarea);
+    listar();
+
+})
