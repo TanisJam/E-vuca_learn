913662b2a211318eb8bf4012f5a556c8aa53a46f
Mauricio Romero <mauricionromero@hotmail.com>
2021-06-18 23:16:20 -0300

    añadida la funcionalidad de borrar, uso de localstorage y algo de css

--- clase_2/script.js
+++ clase_2/script.js
@@ -1,51 +1,81 @@
 //VARIABLES GLOBALES
 const Formulario = document.getElementById("formulario");
 const Listado = document.getElementById("lista");
-let lista = [];
 
 
 
 //FUNCIONES
 function guardar(e) {
-    lista.unshift(e);
+    let tareas = JSON.parse(localStorage.getItem('tareas'));
+    if (tareas) {
+        tareas.unshift(e);
+    } else {
+        tareas = [];
+        tareas.push(e);
+    }
+    localStorage.setItem('tareas', JSON.stringify(tareas));
 }
 
 function listar() {
-    if (lista) {
+    let tareas = JSON.parse(localStorage.getItem('tareas'));
+    if (tareas) {
         Listado.innerHTML = "";
-        lista.forEach(tarea => {
+        tareas.forEach(tarea => {
             let item = document.createElement("div");
             item.className = "tarea";
 
             item.innerHTML = `
             <div class="tarea-cabecera">
                 <p class="tarea-titulo">${tarea.titulo}</p>
-                <p class="tarea-tiempo"> ${tarea.tiempo}m</p>
                 <span class="material-icons tarea-importante ${tarea.esImportante ? '' : 'oculta'}"> priority_high </span>
+                <p class="tarea-tiempo"> ${tarea.tiempo}m</p>
             </div>
-            <div class="tarea-cuerpo">
+            <div class="tarea-cuerpo no-visible">
                 <p class="tarea-descripcion">${tarea.descripcion}</p>
                 <div class="tarea-botones">
-                    <span id="hecho" class="material-icons"> check </span>
-                    <span id="editar" class="material-icons"> edit </span>
-                    <span id="borrar" class="material-icons"> delete </span>
+                    <span id="hecho" class="material-icons boton"> check </span>
+                    <span id="editar" class="material-icons boton"> edit </span>
+                    <span id="borrar" class="material-icons boton" onclick="borrar(this)"> delete </span>
                 </div>
             </div>
-            <span class="material-icons tarea-expandir" onclick="expandir(this)">
-                expand_less
-            </span>`;
+            <span class="material-icons tarea-expandir boton" onclick="expandir(this)">expand_more</span>`;
             Listado.appendChild(item);
         })
     }
 }
 
+function borrar(e) {
+    let tituloTarea = e.parentElement
+        .parentElement
+        .parentElement
+        .querySelector(".tarea-titulo")
+        .innerText;
+
+    let tareas = JSON.parse(localStorage.getItem('tareas'));
+
+    let posicion = tareas.find((tarea, i) =>{
+        if (tarea.titulo === tituloTarea){
+            return i;
+        }
+    })
+
+    tareas.splice(posicion, 1);
+    localStorage.setItem('tareas', JSON.stringify(tareas));
+    listar();
+
+}
+
 let expandir = (e) => {
-    let cuerpo = e.parentElement.querySelector(".tarea-cuerpo"); 
-    let seMuestra = cuerpo.classList.contains("oculta");
-    if (seMuestra){
-        cuerpo.classList.remove("oculta");
+    let cuerpo = e.parentElement.querySelector(".tarea-cuerpo");
+    let seMuestra = cuerpo.classList.contains("visible");
+    if (seMuestra) {
+        cuerpo.classList.remove("visible");
+        cuerpo.classList.add("no-visible");
+        cuerpo.nextSibling.nextSibling.innerText = "expand_more";
     } else {
-        cuerpo.classList.add("oculta");
+        cuerpo.classList.remove("no-visible");
+        cuerpo.classList.add("visible");
+        cuerpo.nextSibling.nextSibling.innerText = "expand_less";
     }
 }
 
@@ -53,6 +83,9 @@ let expandir = (e) => {
 
 
 //EVENT LISTENERS
+
+window.onload = listar;
+
 Formulario.addEventListener('submit', e => {
     e.preventDefault();
 
@@ -71,8 +104,6 @@ Formulario.addEventListener('submit', e => {
     }
 
     guardar(tarea);
-
     data.reset();
     listar();
-
 })
